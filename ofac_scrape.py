9107a6ba44bb6211fd3c05ee52b7e4e1dc738b45
Martín López <martin.lopez@konsigue.com>
2018-08-27 12:08:34 -0500

    Add CaptchaImage class

--- ofac_scrape.py
+++ ofac_scrape.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,28 +7,38 @@ from PIL import Image
 
 import time
 
-import urllib
+class CaptchaImage():
+    #browser = webdriver.Chrome(executable_path='./chromedriver')
 
-browser = webdriver.Firefox(executable_path='/home/konsigue2/Documentos/scraping_SAT/geckodriver')
+    def getCaptchaImage(self, browser):
+        self.browser = browser
+        self.openSATHomePage()
+        self.openDownloadInvoicesPage()
+        self.getDownloadPageScreenshot()
+        self.cropCaptchaImage()
 
-browser.get("https://www.sat.gob.mx/personas/factura-electronica")
+    def openSATHomePage(self):
+        self.browser.get("https://www.sat.gob.mx/personas/factura-electronica")
 
-e=browser.find_element_by_link_text('Cancela y recupera tus facturas')
-e.click()
-browser.switch_to_window(browser.window_handles[1])
-delay = 3 # seconds
-try:
-    WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'buttonFiel')))
-    print("Page is ready!")
-    time.sleep(3)
-    browser.save_screenshot("captcha.png")
-    img = Image.open("captcha.png")
-    area= (75, 325, 317, 425)
-    cropped=img.crop(area)
-    cropped.save("captcha.png", "PNG")
-    img.close()
+    def openDownloadInvoicesPage(self):
+        e = self.browser.find_element_by_link_text('Cancela y recupera tus facturas')
+        e.click()
+        self.browser.switch_to_window(self.browser.window_handles[1])
 
-    print("Captcha saved")
-    browser.quit()
-except TimeoutException:
-    print("Loading took too much time!")
+    def getDownloadPageScreenshot(self):
+        delay = 3 # seconds
+        try:
+            WebDriverWait(self.browser, delay).until(EC.presence_of_element_located((By.ID, 'buttonFiel')))
+            print("Page is ready!")
+            time.sleep(3)
+            self.browser.save_screenshot("captcha.png")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+    def cropCaptchaImage(self):
+        img = Image.open("captcha.png")
+        area= (75, 325, 317, 425)
+        cropped=img.crop(area)
+        cropped.save("captcha.png", "PNG")
+        img.close()
+        print("Captcha saved")
